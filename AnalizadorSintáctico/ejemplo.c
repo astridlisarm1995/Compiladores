5c6dbe41bd51dfbc7c0423c4abbe8209eca9674c
astridlisarm <astrid.rodriguez15@gmail.com>
2019-07-03 14:30:49 -0400

    Analizador Sintáctico

---
+++ AnalizadorSintáctico/ejemplo.c
@@ -0,0 +1,73 @@
+int main(){
+
+	int x, y;
+
+	int p;
+	
+	x= 5+7;
+	x= 0;
+	y = 9;
+
+
+	struct n{
+		int a;
+		float b;
+		char c;
+	};
+
+
+	switch(v){
+	case 5: x = 0; v++; break;
+	case 'a': x = 0; v++; break;
+	}
+
+
+	if (x>0){
+		for (y=1; y<6;y++){
+			x++;
+		}
+	}
+
+	else
+	{
+		y=6;
+		while(y>0){
+			x++;
+			y--;
+		}
+	}
+
+	for (y=1; y<6;y++){
+		for (y=1; y<6;y++){
+			x++;
+		}
+	}	
+
+
+	if (x>0){
+		for (y=1; y<6;y++){
+			x++;
+		}
+	}
+
+	return (0);
+}
+
+int funcion(int i, float f){
+	i=0;
+	f=i+1;
+
+	struct nuevaEstructura{
+		int aX;
+		float flotante;
+		char ccar;
+	};
+
+   return (9);
+}
+
+void nulo(){
+	do{
+		x = x-1;
+	}while(x > 0);
+}
---
+++ AnalizadorSintáctico/funcion_prueba.c
@@ -0,0 +1,16 @@
+int main()
+{
+  int x, y, z, cont
+
+  x = 0;
+   = 0;
+ 
+ 
+  for (cont = 1; cont <= 20; cont = cont+1)
+  
+      z = x+y;
+      x = y;
+      y = z;
+  }
+  return (0);
+}
